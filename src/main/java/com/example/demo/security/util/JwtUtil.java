970ab4b1bf00fee524851267ee7724053b402beb
mica <micaela.dominguez@ing.austral.edu.ar>
2022-09-28 16:55:10 -0300

    Changes for authorizations in endpoints

--- src/main/java/com/example/demo/security/util/JwtUtil.java
+++ src/main/java/com/example/demo/security/util/JwtUtil.java
@@ -3,7 +3,6 @@ package com.example.demo.security.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -47,10 +46,7 @@ public class JwtUtil {
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
-    public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-    }
+
 
     public Boolean validateToken(String token){
         return !isTokenExpired(token);
