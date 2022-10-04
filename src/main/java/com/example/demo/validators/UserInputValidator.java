e151c3f052cb97087584241e2b8335bb1096cf60
mica <micaela.dominguez@ing.austral.edu.ar>
2022-10-04 11:41:17 -0300

    Cambio de mensajes

--- src/main/java/com/example/demo/validators/UserInputValidator.java
+++ src/main/java/com/example/demo/validators/UserInputValidator.java
@@ -5,38 +5,41 @@ import com.example.demo.dto.user.UserUpdateDTO;
 import com.example.demo.utils.Utils;
 
 public class UserInputValidator {
+    private Utils utils = new Utils();
     public boolean checkUpdateInput(UserUpdateDTO userUpdateDTO) throws Exception {
-        final Utils utils = new Utils();
-        if(userUpdateDTO.getUsername() != null){
-            if( ! utils.checkString(userUpdateDTO.getUsername(),3, 16))
-                throw new Exception("Invalid username: should be min length 3 and max length 16");
-
-        }
-        if(userUpdateDTO.getBiography() != null){
-            if(! utils.checkString(userUpdateDTO.getBiography(),0, 100))
-                throw new Exception("Invalid biography : should be max length 100");
-        }
-        if(userUpdateDTO.getPassword() != null){
-            if( !utils.checkString(userUpdateDTO.getPassword(),8, 20))
-                throw new Exception("Invalid password: should be min length 8 and max length 20");
-        }
-        return true;
+        return checkUsername(userUpdateDTO.getUsername()) && checkBiography(userUpdateDTO.getBiography()) && checkPassword(userUpdateDTO.getPassword());
     }
 
     public boolean checkCreationInput(UserCreationDTO user) throws Exception {
         if (user.getUsername() == null || user.getPassword() == null || user.getEmail() == null)
-            throw new Exception("Missing fields");
+            throw new Exception("Campos faltantes");
         else {
-            if (!Utils.checkString(user.getUsername(), 3, 16))
-                throw new Exception("Invalid username: should be min length 3 and max length 16");
-            if(user.getBio() != null){
-                if (!Utils.checkString(user.getBio(), 0, 100))
-                    throw new Exception("Invalid biography : should be max length 100");
-            }
-            if (!Utils.checkString(user.getPassword(), 8, 20))
-                throw new Exception("Invalid password: should be min length 8 and max length 20");
-            if (!Utils.isEmail(user.getEmail())) throw new IllegalArgumentException("Email format invalid");
-            return true;
+            if (!Utils.isEmail(user.getEmail())) throw new IllegalArgumentException("Invalido formato de email");
+            return checkUsername(user.getUsername()) && checkBiography(user.getBio()) && checkPassword(user.getPassword());
         }
+    }
+
+    private boolean checkUsername(String username) throws Exception {
+        if(username != null){
+            if( ! utils.checkString(username,3, 16))
+                throw new Exception("El nombre de usuario debe tener entre 3 y 16 caracteres");
+        }
+        return true;
+    }
+
+    private boolean checkBiography(String biography) throws Exception {
+        if(biography != null){
+            if(!utils.checkString(biography,0, 100))
+                throw new Exception("Largo maximo de la Bio 100 caracteres");
+        }
+        return true;
+    }
+
+    private boolean checkPassword(String password) throws Exception {
+        if(password != null){
+            if( !utils.checkString(password,8, 20))
+                throw new Exception("La contraseña debe tener entre 8 y 20 caracteres");
+        }
+        return true;
     }
 }
